1ddaa7fef8378714bc1a9ada0781d7d2457a295c
Vishal Kashi <dev.vishalkashi@gmail.com>
2023-07-02 14:29:03 +0530

    feat: about page

---
+++ app/about/UserLink.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+interface UserLinkProps {
+  name: string;
+  description: string;
+  link: string;
+  image?: string;
+}
+
+const UserLink: React.FC<UserLinkProps> = (props) => {
+  const { name, description, link, image } = props;
+  return (
+    <Link href={link} className="w-full">
+      <Button
+        variant="ghost"
+        className="w-full h-fit p-4 flex flex-row justify-start rounded-lg hover:dark:bg-zinc-700"
+      >
+        <Image
+          src={image || "/user_blank_2.png"}
+          alt=""
+          height={60}
+          width={60}
+          className="rounded-full"
+        />
+        <div className="ml-4">
+          <p className="text-md text-left">{name}</p>
+          <p className="text-md text-left text-zinc-400 font-normal">
+            {description}
+          </p>
+        </div>
+      </Button>
+    </Link>
+  );
+};
+
+export default UserLink;
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import UserLink from "./UserLink";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
+import { Card } from "@/components/ui/card";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <Card className="w-full flex flex-col justify-center p-6 dark:bg-zinc-800 dark:border-none">
+        <div className="w-full flex justify-center">
+          <Image
+            src="https://github.com/destructo570.png"
+            alt="@shadcn"
+            height={100}
+            width={100}
+            className="rounded-full"
+          />
+        </div>
+        <p className="mt-6 text-sm">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+          tincidunt metus eget lorem pellentesque ultrices. Duis non dapibus
+          leo. Nulla nec eleifend lectus, in porttitor risus. Praesent tristique
+          porttitor lacinia. Ut ultricies gravida tortor a porttitor. Vivamus
+          semper euismod cursus. Mauris convallis, purus congue sagittis auctor,
+          odio arcu maximus risus, pretium fringilla tortor odio imperdiet
+        </p>
+      </Card>
+      <Card className="p-6 mt-4 dark:bg-zinc-800 dark:border-none ">
+        <h3 className="font-bold">Credits</h3>
+        <div className="grid md:grid-cols-2 gap-2 mt-4">
+          <UserLink
+            name="Shadcn"
+            description="UI components library"
+            link="https://twitter.com/shadcn"
+            image="/shadcn_profile.jpg"
+          />
+          <UserLink
+            name="DJTKana"
+            description="Image and audio assets"
+            link="https://djtguide.neocities.org/"
+          />
+        </div>
+      </Card>
+    </>
+  );
 };
 
 export default About;
